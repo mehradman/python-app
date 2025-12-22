eafc74a2a3fa05bfe4a2d5e054aad38c0519ee3f
mehradman <m.radmanmehr98@gmail.com>
2025-12-22 06:51:53 +0330

    Edit CI workflow

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
         {
             'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
             'hostname': socket.gethostname(),
-            'message': 'mashalla!'
+            'message': 'mashalla!!'
         }
     )
 
