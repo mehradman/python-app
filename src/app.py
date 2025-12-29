c94df0eff284d41d1ef58e1f23d63698a591b502
mehradman <m.radmanmehr98@gmail.com>
2025-12-29 13:41:02 +0330

    Edit CD pipeline

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
         {
             'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
             'hostname': socket.gethostname(),
-            'message': 'You are doing great, little human!!'
+            'message': 'You are doing great, little human!'
         }
     )
 
